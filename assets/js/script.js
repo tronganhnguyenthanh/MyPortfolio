a0b2ec7a577fcd560ebcc6b2d01bc32612e77ca6
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-09-15 16:34:38 +0700

    [UPDATE][FE] update layout version 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,14 @@ displayColorInput()
 
 function getInputValue(){
   let value = document.querySelector("#input_value").value
+  document.querySelector(".aboutMe").style.backgroundColor = value
+  document.querySelector(".aboutMe").style.padding = "4px"
+  document.querySelector(".aboutMe").style.textAlign = "center"
+  document.querySelector(".aboutMe").style.color = "#fff"
+  document.querySelector(".section-objective").style.backgroundColor = value
   document.querySelector(".section-objective > h2").style.color = "#fff"
   document.querySelector(".section-objective > p").style.color = "#fff"
-  document.querySelector(".workExp").style.color = value
-  document.querySelector(".section-skills > h2").style.color = "#fff"
-  document.querySelector("body").style.backgroundColor = value
-  document.querySelector(".about-me").style.color = "#fff"
-  document.querySelector(".aboutMe").style.color = "#fff"
-  document.querySelector(".introduction").style.color = "#fff"
-  document.querySelector(".project-content").style.color = "#fff"
-  document.querySelector("#project-content-second").style.color = "#fff"
+  document.querySelector(".section-objective > p").style.textAlign = "center"
+  document.querySelector(".section-objective > h2").style.textAlign = "center"
+  document.querySelector(".section-skills").style.backgroundColor = value
 }
