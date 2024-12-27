a60c961135e10dbfdf6266c76bdda6b3ae26ba51
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-12-27 11:46:34 +0700

    [UPDATE][FE] change style in personal project section

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,39 @@ function getInputValue(){
   document.querySelector(".project-name").style.color = "#fff"
   document.querySelector(".section-skills > h2").style.color = "#fff"
 }
+
+function getProjectCategory(){
+  let projects = [
+   {
+     name:"Candidate Management Sytem",
+     urlLink:"https://candidates-beta.vercel.app",
+     bgColor:"dodgerblue"
+   },
+   {
+     name:"Course Management System",
+     urlLink:"https://new-courseupdate.surge.sh",
+     bgColor:"#ffa233"
+   },
+   {
+     name:"A funny quiz",
+     urlLink:"https://quiz-api-call.vercel.app/login.html",
+     bgColor:"darkblue"
+   }
+  ]
+  let prj = ""
+  projects?.forEach(function(i){
+   prj += `
+    <details>
+      <summary style="background-color:${i?.bgColor}">${i?.name}</summary>
+      <ul>
+       <li>
+         <a href="${i?.urlLink}">${i?.urlLink}</a>
+       </li>
+      </ul>
+    </details>
+   `
+  })
+  document.querySelector("#personal_projects").innerHTML = prj
+}
+
+getProjectCategory()
